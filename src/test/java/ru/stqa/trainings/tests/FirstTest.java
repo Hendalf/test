15fb82d7b1e757a039e185bbffcdba8bae700c29
Vladyslav Revidinskyy <revidinskyy@gmail.com>
2016-04-25 00:07:36 +0300

    Добавил проект и скриншот с запуском теста из консоли

---
+++ src/test/java/ru/stqa/trainings/tests/FirstTest.java
@@ -0,0 +1,14 @@
+package ru.stqa.trainings.tests;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Vlad on 24.04.2016.
+ */
+public class FirstTest {
+
+  @Test
+  public void testFirstTest(){
+    System.out.println("Test was passed!!!");
+  }
+}
